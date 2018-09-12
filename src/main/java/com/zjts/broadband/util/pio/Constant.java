25ae35f04679f0929224211b17c9951386c7e41a
chenfs <chenfs@192.168.88.6:29418>
2018-09-12 14:33:39 +0800

    添加excel工具类

---
+++ src/main/java/com/zjts/broadband/util/pio/Constant.java
@@ -0,0 +1,8 @@
+package com.zjts.broadband.util.pio;
+
+public class Constant {
+
+	static final String XLSX_SUFFIX = ".xlsx";
+
+	static final String XLSX_CONTENT_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+}
