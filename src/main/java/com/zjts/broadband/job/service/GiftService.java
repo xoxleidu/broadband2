717076d6f5fe6cc85493947f330f228eaa663144
wangxc <904211340@qq.com>
2018-09-18 10:53:55 +0800

    @Service

--- src/main/java/com/zjts/broadband/job/service/GiftService.java
+++ src/main/java/com/zjts/broadband/job/service/GiftService.java
@@ -4,10 +4,11 @@ import com.zjts.broadband.common.model.APIResponse;
 import com.zjts.broadband.common.model.req.job.project.ReqGiftAdd;
 import com.zjts.broadband.common.model.req.job.project.ReqGiftQuery;
 import com.zjts.broadband.job.model.Gift;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Service
 public interface GiftService {
     APIResponse update(ReqGiftQuery reqQuery);
 
