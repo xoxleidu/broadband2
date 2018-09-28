197d956dce08fae7c72d693f7c718df0dc6edd8e
210gwl <18610588369@163.com>
2018-09-28 08:43:46 +0800

    订单初级

--- src/main/java/com/zjts/broadband/job/service/serviceimpl/OrdersServiceImpl.java
+++ src/main/java/com/zjts/broadband/job/service/serviceimpl/OrdersServiceImpl.java
@@ -4,22 +4,22 @@ import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.zjts.broadband.common.constant.CodeEnum;
 import com.zjts.broadband.common.model.APIResponse;
+import com.zjts.broadband.common.model.req.job.orders.ReqOrderAdd;
 import com.zjts.broadband.common.model.req.job.orders.ReqOrderMiddleQuery;
 ;
 import com.zjts.broadband.job.dao.OrdersMapper;
+import com.zjts.broadband.job.model.Equipment;
+import com.zjts.broadband.job.model.OrderInsert;
 import com.zjts.broadband.job.model.Orders;
 
-import com.zjts.broadband.job.model.Orders_Expenses;
+import com.zjts.broadband.job.model.OrdersAll;
 import com.zjts.broadband.job.service.OrdersService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class OrdersServiceImpl implements OrdersService {
@@ -35,7 +35,7 @@ public class OrdersServiceImpl implements OrdersService {
      */
     @Override
     public APIResponse findByProductId(Integer id) throws Exception {
-        List<Orders_Expenses> list = new ArrayList<Orders_Expenses>();
+        List<OrdersAll> list = new ArrayList<OrdersAll>();
         try {
             list = ordersMapper.selectByProductId(id);
         } catch (Exception e) {
@@ -53,9 +53,9 @@ public class OrdersServiceImpl implements OrdersService {
      */
     @Override
     public Page<Map> findSelectAll(Page<Map> mapPage, ReqOrderMiddleQuery reqOrderMiddleQuery) throws Exception {
-        Orders_Expenses orderQuery = new Orders_Expenses();
-        BeanUtils.copyProperties(reqOrderMiddleQuery, orderQuery);
-        mapPage.setRecords(ordersMapper.selectCustomerOrderAll(mapPage, orderQuery));
+        OrdersAll ordersAll = new OrdersAll();
+        BeanUtils.copyProperties(reqOrderMiddleQuery, ordersAll);
+        mapPage.setRecords(ordersMapper.selectOrderAll(mapPage, ordersAll));
         return mapPage;
     }
 
@@ -67,12 +67,13 @@ public class OrdersServiceImpl implements OrdersService {
      * @throws: Exception
      */
     @Override
-    public APIResponse updateMoney(ReqOrderMiddleQuery reqOrderMiddleQuery) {
+    public APIResponse updateMoney(ReqOrderMiddleQuery reqOrderMiddleQuery) throws Exception{
         Orders orders = new Orders();
         BeanUtils.copyProperties(reqOrderMiddleQuery, orders);
         Integer insert = 1;//= orderMapper.updateMoney(order);
         EntityWrapper<Orders> entityWrapper = new EntityWrapper<Orders>();
         entityWrapper.setEntity(new Orders());
+        orders.setId(reqOrderMiddleQuery.getOrderId());
         entityWrapper.setSqlSelect("money").where("customer_id={0}", orders.getCustomerId()).and("id={0}", orders.getId());
         List<Orders> list = ordersMapper.selectList(entityWrapper);
         for (Orders or : list
@@ -85,40 +86,75 @@ public class OrdersServiceImpl implements OrdersService {
         }
         return APIResponse.success(insert);
     }
-
     /**
-     * 根据用户ID ，资费ID,套餐ID,设备ID，赠品ID进行详细查询
+     * 根据用户ID和产品ID产生订单
      *
-     * @param: ReqOrderMiddleQuery类型的对象
+     * @param: ReqOrderAdd类型的对象
      * @return: 返回到 APIResponse 类型的的对象
      * @throws: Exception
      */
     @Override
-    public APIResponse selectByCustomerAll(ReqOrderMiddleQuery reqOrderMiddleQuery) {
-        Orders_Expenses orders_expenses = new Orders_Expenses();
-        BeanUtils.copyProperties(reqOrderMiddleQuery, orders_expenses);
-        List list = new ArrayList<>();
-        list.add(ordersMapper.selectById(reqOrderMiddleQuery.getCustomerId()));
-
-        return APIResponse.success(list);
-    }
-
-    @Override
-    public APIResponse orderInsert(ReqOrderMiddleQuery reqOrderMiddleQuery) {
-        Orders_Expenses orders_expenses = new Orders_Expenses();
+    public APIResponse orderInsert(ReqOrderAdd reqOrderAdd)  throws Exception{
+        OrderInsert orderInsert = new OrderInsert();
         Orders orders =new Orders();
-        BeanUtils.copyProperties(reqOrderMiddleQuery, orders_expenses);
-        BeanUtils.copyProperties(reqOrderMiddleQuery, orders);
+        List list=new ArrayList<>();
+        BeanUtils.copyProperties(reqOrderAdd, orderInsert);
+        BeanUtils.copyProperties(reqOrderAdd, orders);
         int a[] = new int[10];
         //订单号
-        orders_expenses.setOrderNumber((int) (10 * (Math.random())));
+        orders.setOrderNumber(GetRandomString(20));
         //订单创建时间
-        Integer time = Math.toIntExact(new Date().getTime() / 1000);
-           int relust= ordersMapper.insertOrder(orders_expenses);
-           int  insert =ordersMapper.insert(orders);
-        if (insert != 1 && relust!=1) {
-            return APIResponse.error(CodeEnum.SAVE_ERROR);
+        orders.setCreateTime( Math.toIntExact(new Date().getTime() / 1000));
+         //  int relust= ordersMapper.insertOrder(orderInsert);
+        for (Object o: orderInsert.getEquipmentId()
+             ) {
+
         }
+        for (Object orderInsert1: orderInsert.getGiftId()
+             ) {
+            list.add(orderInsert1);
+        }
+        for (Object l:list
+             ) {
+            System.out.print(l);
+        }
+         //  int  insert =ordersMapper.insert(orders);
+        //if (insert != 1 && relust!=1) {
+        //    return APIResponse.error(CodeEnum.SAVE_ERROR);
+    //    }
         return APIResponse.success();
     }
+
+/**
+ *
+ * 生成随机的订单号
+ * */
+    public static String GetRandomString(int Len) {
+
+        String[] baseString={"0","1","2","3",
+                "4","5","6","7","8","9",
+                "a","b","c","d","e",
+                "f","g","h","i","j",
+                "k","l","m","n","o",
+                "p","q","r","s","t",
+                "u","v","w","x","y",
+                "z","A","B","C","D",
+                "E","F","G","H","I",
+                "J","K","L","M","N",
+                "O","P","Q","R","S",
+                "T","U","V","W","X","Y","Z"};
+        Random random = new Random();
+        int length=baseString.length;
+        String randomString="";
+        for(int i=0;i<length;i++){
+            randomString+=baseString[random.nextInt(length)];
+        }
+        random = new Random(System.currentTimeMillis());
+        String resultStr="";
+        for (int i = 0; i < Len; i++) {
+            resultStr += randomString.charAt(random.nextInt(randomString.length()-1));
+        }
+        return resultStr;
+    }
+
 }
