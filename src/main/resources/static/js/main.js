684e9f6a0e5c173f2c310afd412b51cacfdd2698
dulei <dul>
2018-10-10 15:03:48 +0800

    前端模板

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,146 @@
+layui.config({
+	base : "js/"
+}).use(['form','element','layer','jquery'],function(){
+	var form = layui.form(),
+		layer = parent.layer === undefined ? layui.layer : parent.layer,
+		element = layui.element(),
+		$ = layui.jquery;
+
+	$(".panel a").on("click",function(){
+		window.parent.addTab($(this));
+	})
+
+	//动态获取文章总数和待审核文章数量,最新文章
+	$.get("../json/newsList.json",
+		function(data){
+			var waitNews = [];
+			$(".allNews span").text(data.length);  //文章总数
+			for(var i=0;i<data.length;i++){
+				var newsStr = data[i];
+				if(newsStr["newsStatus"] == "待审核"){
+					waitNews.push(newsStr);
+				}
+			}
+			$(".waitNews span").text(waitNews.length);  //待审核文章
+			//加载最新文章
+			var hotNewsHtml = '';
+			for(var i=0;i<5;i++){
+				hotNewsHtml += '<tr>'
+		    	+'<td align="left">'+data[i].newsName+'</td>'
+		    	+'<td>'+data[i].newsTime+'</td>'
+		    	+'</tr>';
+			}
+			$(".hot_news").html(hotNewsHtml);
+		}
+	)
+
+	//图片总数
+	$.get("../json/images.json",
+		function(data){
+			$(".imgAll span").text(data.length);
+		}
+	)
+
+	//用户数
+	$.get("../json/usersList.json",
+		function(data){
+			$(".userAll span").text(data.length);
+		}
+	)
+
+	//新消息
+	$.get("../json/message.json",
+		function(data){
+			$(".newMessage span").text(data.length);
+		}
+	)
+
+    var myChartBing = echarts.init(document.getElementById('yonghubing'));
+
+	//饼状图
+    myChartBing.setOption({
+        series : [
+            {
+                name: '用户来源',
+                type: 'pie',
+                radius: '55%',
+                data:[
+                    {value:235, name:'视频广告'},
+                    {value:274, name:'联盟广告'},
+                    {value:310, name:'邮件营销'},
+                    {value:335, name:'直接访问'},
+                    {value:400, name:'搜索引擎'}
+                ]
+            }
+        ]
+    })
+
+	//柱状图
+    // 基于准备好的dom，初始化echarts实例
+    var myChartZhu = echarts.init(document.getElementById('dingdanzhu'));
+
+	// 指定图表的配置项和数据
+		var option = {
+			title: {
+				text: ''
+			},
+			tooltip: {},
+			legend: {
+				data:['销量']
+			},
+			xAxis: {
+				data: ["宽带","固话","电视","光猫","路由","IPTV"]
+			},
+			yAxis: {},
+			series: [{
+				name: '销量',
+				type: 'bar',
+				data: [5, 20, 36, 10, 10, 20]
+			}]
+		};
+
+	// 使用刚指定的配置项和数据显示图表。
+    myChartZhu.setOption(option);
+
+	//数字格式化
+	$(".panel span").each(function(){
+		$(this).html($(this).text()>9999 ? ($(this).text()/10000).toFixed(2) + "<em>万</em>" : $(this).text());	
+	})
+
+	//系统基本参数
+	if(window.sessionStorage.getItem("systemParameter")){
+		var systemParameter = JSON.parse(window.sessionStorage.getItem("systemParameter"));
+		fillParameter(systemParameter);
+	}else{
+		$.ajax({
+			url : "../json/systemParameter.json",
+			type : "get",
+			dataType : "json",
+			success : function(data){
+				fillParameter(data);
+			}
+		})
+	}
+
+	//填充数据方法
+ 	function fillParameter(data){
+ 		//判断字段数据是否存在
+ 		function nullData(data){
+ 			if(data == '' || data == "undefined"){
+ 				return "未定义";
+ 			}else{
+ 				return data;
+ 			}
+ 		}
+ 		$(".version").text(nullData(data.version));      //当前版本
+		$(".author").text(nullData(data.author));        //开发作者
+		$(".homePage").text(nullData(data.homePage));    //网站首页
+		$(".server").text(nullData(data.server));        //服务器环境
+		$(".dataBase").text(nullData(data.dataBase));    //数据库版本
+		$(".maxUpload").text(nullData(data.maxUpload));    //最大上传限制
+		$(".userRights").text(nullData(data.userRights));//当前用户权限
+ 	}
+
+})
+
+
